066f7e4b0b1a241494e978dea0c47f75ef07c6c3
LourensT <touwenlourens@gmail.com>
2019-06-27 22:08:34 +0200

    histogram for both airlines

--- data_wrangler.py
+++ data_wrangler.py
@@ -145,30 +145,42 @@ class DataWrangler:
         full_df['created_at'] = pd.to_datetime(full_df['created_at'], format='%a %b %d %H:%M:%S +0000 %Y')
         full_df = full_df.sort_values(by='created_at', ascending=False)
 
-        response = {}
-        replytime_series = []
+        airlineids = [str(n) for n in airlineids]
+
+        klm_dict = {}
+        ba_dict = {}
+
+        klm_series = []
+        ba_series = []
+
         for data in full_df[["('user', 'id_str')", 'id_str', 'in_reply_to_status_id', 'created_at']].values:
-            if str(data[0]) == str(airlineids):
+            if str(data[0]) == airlineids[0]:
                 try:
-                    response[int(data[2])] = data[3]
+                    klm_dict[int(data[2])] = data[3]
+                except ValueError:
+                    pass
+            elif str(data[0]) == airlineids[1]:
+                try:
+                    ba_dict[int(data[2])] = data[3]
                 except ValueError:
                     pass
             else:
-                if int(data[1]) in response.keys():
-                    if response[int(data[1])] > data[3]:
-                        td = self.timedelta(data[3], response[int(data[1])])
-                        replytime_series.append(td)
+                if int(data[1]) in klm_dict.keys():
+                    if klm_dict[int(data[1])] > data[3]:
+                        td = self.timedelta(data[3], klm_dict[int(data[1])])
+                        klm_series.append(td)
+                if int(data[1]) in ba_dict.keys():
+                    if ba_dict[int(data[1])] > data[3]:
+                        td = self.timedelta(data[3], ba_dict[int(data[1])])
+                        ba_series.append(td)
 
-        # #print(response)
-        # print(replytime_series)
-        # print(len(replytime_series))
-
-        with open('replytimefile', 'wb') as fp:
-            pickle.dump(replytime_series, fp)
+        pickle_dictionary = {"KLM": klm_series, "BA" : ba_series}
+        with open('replytime_extracted', 'wb') as fp:
+            pickle.dump(pickle_dictionary, fp)
 
 
 
 if __name__ == '__main__':
     # For testing
     wrangler = DataWrangler()
-    wrangler.replytime_wrangle(airlineids=56377143)
+    wrangler.replytime_wrangle(airlineids=[56377143,18332190])
--- final.py
+++ final.py
@@ -36,10 +36,10 @@ class Demo:
         plt.tight_layout()
         plt.savefig('sent_result.png', dpi=300)
 
-    def replytime_hist(self, replytimelist):
+    def replytime_hist(self, replytime_data):
         fig, ax = plt.subplots(nrows=1, ncols=1)
-        plt.hist(replytimelist, normed=True, color ='b', alpha=0.5, bins=500, label="KLM")
-        #plt.hist(df_ba1['replyTime'], normed=True, color ='g', alpha=0.5, bins=500, label="British Airways")
+        plt.hist(replytime_data["KLM"], normed=True, color ='b', alpha=0.5, bins=500, label="KLM")
+        plt.hist(replytime_data["BA"], normed=True, color ='g', alpha=0.5, bins=500, label="British Airways")
         plt.xlim(0, 21600)
         plt.legend(prop={'size': 14})
         plt.xticks([0, 3600, 7200, 10800, 14400, 18000, 21600], ['0 hours', '1 hour', '2 hours', '3 hours', '4 hours', '5 hours', '6 hours'])
@@ -64,13 +64,15 @@ class Demo:
         extractor.save_csv()
         print('Creating new features')
         wrangler = DataWrangler()
-        wrangler.replytime_wrangle(airlineids=56377143)
+        series = {}
+        wrangler.replytime_wrangle(airlineids=[56377143,18332190])
         print('Making plot')
 
         #read in replytimedata
-        with open ('replytimefile', 'rb') as fp:
-            replytimelist = pickle.load(fp)
-        self.replytime_hist(replytimelist)
+        with open ('replytime_extracted', 'rb') as fp:
+            replytime_data = pickle.load(fp)
+        #print(type(replytime_data['KLM']))
+        self.replytime_hist(replytime_data)
 
     def demo_sentiment(self):
         """
